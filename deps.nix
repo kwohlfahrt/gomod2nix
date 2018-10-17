9cbbcf8c6d04880ffebd58aa1bd8b61dd18f250c
Kai Wohlfahrt <kai@prodo.ai>
2018-10-17 14:32:03 +0100

    Add deps.nix
    Solves bootstrap problem, and is a useful integration test.

---
+++ deps.nix
@@ -0,0 +1,9 @@
+[{
+  goPackagePath = "golang.org/x/tools";
+  fetch = {
+    type = "git";
+    url = "https://go.googlesource.com/tools";
+    rev = "59602fdee893255faef9b2cd3aa8f880ea85265c";
+    sha256 = "09xdxkrq1pr226zh7d94qnmqdcl5igdlinf1h3l839i4r9lp12zy";
+  };
+}]
